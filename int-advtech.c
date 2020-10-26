d4ca24508e53354af10520191d3084414b0636bb
mfld-fr <mfld.fr@gmail.com>
2020-10-26 21:52:43 +0100

    Remove useless Advantech BIOS handlers
    Use real BIOS INT 10h, 16h and 17h,
    with underlying emulated serial device.
    
    Issue #14

--- int-advtech.c
+++ int-advtech.c
@@ -56,132 +56,6 @@ int int_io_write (word_t p, word_t w)
 // Interrupt handlers
 //------------------------------------------------------------------------------
 
-// BIOS video services
-// Tweaked by Advantech
-// Redirected to serial port
-
-static byte_t num_hex (byte_t n)
-	{
-	byte_t h = n + '0';
-	if (h > '9') h += 'A' - '9';
-	return h;
-	}
-
-static int int_10h ()
-	{
-	byte_t ah = reg8_get (REG_AH);
-
-	byte_t al;
-
-	word_t es;
-	word_t bp;
-	word_t cx;
-
-	addr_t a;
-	byte_t c;
-
-	switch (ah)
-		{
-		// Print character
-
-		case 0x0A:
-			serial_send (reg8_get (REG_AL));
-			break;
-
-		// Print string
-
-		case 0x13:
-			es = seg_get (SEG_ES);
-			bp = reg16_get (REG_BP);
-			cx = reg16_get (REG_CX);
-			a = addr_seg_off (es, bp);
-
-			while (cx--)
-				{
-				serial_send (mem_read_byte (a++));
-				}
-
-			break;
-
-		// Write byte as hexadecimal
-
-		case 0x1D:
-			al = reg8_get (REG_AL);
-			c = num_hex (al >> 4);
-			serial_send (c);
-			c = num_hex (al & 0x0F);
-			serial_send (c);
-			break;
-
-		default:
-			printf ("fatal: INT 10h: AH=%hxh not implemented\n", ah);
-			assert (0);
-		}
-
-	return 0;
-	}
-
-// BIOS keyboard services
-// Tweaked by Advantech
-// Redirected to serial port
-
-static int int_16h ()
-	{
-	int err = 0;
-	byte_t c = 0;
-
-	byte_t ah = reg8_get (REG_AH);
-	switch (ah)
-		{
-		// Keyboard initialization
-		// Do not intercept BIOS
-
-		case 0x00:
-			err = 1;
-			break;
-
-		// Read next char (blocking)
-
-		case 0x10:
-			err = serial_recv (&c);
-			if (err) break;
-
-			reg8_set (REG_AL, (byte_t) c);  // ASCII code
-			reg8_set (REG_AH, 0);           // No scan code
-			break;
-
-		default:
-			printf ("fatal: INT 16h: AH=%hxh not implemented\n", ah);
-			assert (0);
-		}
-
-	return err;
-	}
-
-
-// BIOS printer services
-// Tweaked by Advantech
-// Redirected to serial port
-
-static int int_17h ()
-	{
-	byte_t ah = reg8_get (REG_AH);
-	switch (ah)
-		{
-		// Serial reset
-
-		case 0x06:
-			break;
-
-		default:
-			printf ("fatal: INT 17h: AH=%hxh not implemented\n", ah);
-			assert (0);
-		}
-
-	return 0;
-	}
-
-
 // BIOS time services
 
 static int int_1Ah ()
@@ -245,7 +119,7 @@ static int int_D0h ()
 		// ADVTECH: program select (DIP SW) ?
 
 		case 0x01:
-			reg16_set (REG_AX, 3);  // program 01 -> MON86  11 -> TEST
+			reg16_set (REG_AX, 1);  // program 01 -> MON86  11 -> TEST
 			break;
 
 		// No RTC emulated in EMU86
@@ -304,9 +178,6 @@ static int int_D2h ()
 
 int_num_hand_t _int_tab [] = {
 	{ 0x03, int_03h },
-	{ 0x10, int_10h },
-	{ 0x16, int_16h },
-	{ 0x17, int_17h },
 	{ 0x1A, int_1Ah },
 	{ 0x60, int_60h },
 	{ 0xD0, int_D0h },
@@ -320,3 +191,4 @@ int_num_hand_t _int_tab [] = {
 void int_init (void)
 	{
 	}
+
