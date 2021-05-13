4b08cd657e08ebe4bbc394c841476dac99158c83
mfld-fr <mfld.fr@gmail.com>
2021-05-13 12:43:49 +0200

    [advantech] Tune memory and ROM stubs

--- mem-advtech.c
+++ mem-advtech.c
@@ -1,14 +1,27 @@
 
 #include "emu-mem-io.h"
 
+#include <stdio.h>
+
+
 // Memory access
 
 void mem_write_byte (addr_t a, byte_t b, byte_t init)
 	{
-	// No memory mapped device
+	// Small glitch in Advantech original BIOS
 
-	mem_write_byte_0 (a, b, init);
+	if (a == 0xF0692)
+		{
+		puts ("\nwarning: writing byte into ROM @ F0692h");
+		}
+	else
+		{
+		// No memory mapped device
+
+		mem_write_byte_0 (a, b, init);
+		}
 	}
+
 
 void mem_write_word (addr_t a, word_t w, byte_t init)
 	{
